7deee519daf25fedbf0776c186c79df893b46a5e
Jun Ahn <wtg6471@gmail.com>
2023-09-20 16:26:32 -0700

    project reset

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Navbar from '@/components/navbar'
 export default function Home() {
   return (
     <div>
-      <Navbar/>
+      
     </div>
   )
 }
--- components/navbar.js
+++ components/navbar.js
@@ -2,8 +2,9 @@ import styles from "./stylesheets/navbar.module.css"
 import Image from "next/image"
 
 export default function Navbar(){
+    
     return (
-        <div>
+        <div className={styles.navbar}>
             <div></div>
             <div className={styles.middle}>
                 <Image
@@ -11,8 +12,28 @@ export default function Navbar(){
                     width={300}
                     height={183}
                     alt="company logo"
-                    
+                    className={styles["company-logo"]}
                 />
+                <div className={styles["search-bar"]}>
+                    <i class="fa fa-search"></i>
+                    <input type="text" placeholder="Search" name="search"/>
+                    <button type="submit">search</button>
+                </div>
+                <div className={styles["shopping-cart"]}>
+                    <div className={styles["shopping-cart-left"]}>
+                        <Image
+                            src="/bag.svg"
+                            width={34}
+                            height={34}
+                            alt="bag"
+                        />
+                        <p className={styles.quantity}>2</p>
+                    </div>
+                    <div className={styles["shopping-cart-right"]}>
+                        <p>Shopping cart:</p>
+                        <p className={styles.price}>$57.00</p>
+                    </div>
+                </div>
             </div>
             <div></div>
             <h1>hello</h1>
