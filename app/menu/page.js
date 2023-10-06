eea063d2fe4c22e3d77966fec3b130f0f3a02766
Jun Ahn <wtg6471@gmail.com>
2023-10-05 20:42:24 -0700

    navbar created

---
+++ app/menu/page.js
@@ -0,0 +1,13 @@
+import Heading from "@/components/menu/heading";
+import Item from "@/components/menu/item";
+import Navbar from "@/components/menu/menu-navbar";
+
+export default function Menu() {
+    return(
+        <div>
+            <Heading/>
+            <Item/>
+            <Navbar/>
+        </div>
+    )
+}
