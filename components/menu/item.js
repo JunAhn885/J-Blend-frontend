b51761cd8b0f23441844599a79eed78c5d2f9baa
Jun Ahn <wtg6471@gmail.com>
2023-10-13 15:47:30 -0700

    created separated file for menu items and added unique id for each menu item

--- components/menu/item.js
+++ components/menu/item.js
@@ -3,101 +3,11 @@ import styles from 'components/stylesheets/item.module.css'
 import Image from "next/image"
 import ItemModal from './item-modal'
 import { useState, useEffect } from 'react'
+import { menu_item } from '@/public/menu_item'
 
 export default function Item() {
-    // currently a fake data, this will be retrieved using an API call
-    const menu_item = [
-        {"Donburi": [
-            {
-                "Name": "Chirashi",
-                "Description": "tuna, chopped fatty tuna, salmon, ocean trout, albacore, shrimp, fresh water eel, masago with fresh wasabi",
-                "Price": 18.95
-            },
-            {
-                "Name": "Sake Don",
-                "Description": "salmon with nikiri jouyu (umami soy sauce) with fresh wasabi + yuzu kosho (pepper)",
-                "Price": 15.00
-            },
-            {
-                "Name": "Poke",
-                "Description": "tuna, salmon, crab salad, seaweed salad, cucumber, masago choice of sauce: yuzu (Japanese citrus) soy or sweet spicy",
-                "Price": 15.95
-            },
-            {
-                "Name": "Salmon Poke",
-                "Description": "salmon, crab salad, seaweed salad, cucumber, masago choice of sauce: yuzu (Japanese citrus) soy or sweet spicy",
-                "Price": 16.45
-            }
-        ]},
-        {"Salad": [
-            {
-                "Name": "Crab & Shrimp Salad",
-                "Description": "crab, shrimp, masago, cucumber, tomato, greens with citrus mayo or ginger dressing",
-                "Price": 9.95
-            },
-            {
-                "Name": "Green Salad",
-                "Description": "mixed greens, tomato, cucumber, ginger dressing",
-                "Price": 6.95
-            },
-            {
-                "Name": "Crab & Shrimp Salad",
-                "Description": "crab, shrimp, masago, cucumber, tomato, greens with citrus mayo or ginger dressing",
-                "Price": 9.95
-            },
-            {
-                "Name": "Crab & Shrimp Salad",
-                "Description": "crab, shrimp, masago, cucumber, tomato, greens with citrus mayo or ginger dressing",
-                "Price": 9.95
-            },
-            {
-                "Name": "Crab & Shrimp Salad",
-                "Description": "crab, shrimp, masago, cucumber, tomato, greens with citrus mayo or ginger dressing",
-                "Price": 9.95
-            },
-            {
-                "Name": "Crab & Shrimp Salad",
-                "Description": "crab, shrimp, masago, cucumber, tomato, greens with citrus mayo or ginger dressing",
-                "Price": 9.95
-            },
-            {
-                "Name": "Crab & Shrimp Salad",
-                "Description": "crab, shrimp, masago, cucumber, tomato, greens with citrus mayo or ginger dressing",
-                "Price": 9.95
-            }
-        ]},
-        {"Soup": [
-            {
-                "Name": "Miso Soup",
-                "Description": "aka (red) + shiro (white) miso, tofu, seaweed, scallions",
-                "Price": 2.25
-            },
-            {
-                "Name": "Asari Miso Soup",
-                "Description": "manila clam, scallions",
-                "Price": 3.25
-            },
-            {
-                "Name": "Miso Soup",
-                "Description": "aka (red) + shiro (white) miso, tofu, seaweed, scallions",
-                "Price": 2.25
-            },
-            {
-                "Name": "Miso Soup",
-                "Description": "aka (red) + shiro (white) miso, tofu, seaweed, scallions",
-                "Price": 2.25
-            },
-            {
-                "Name": "Miso Soup",
-                "Description": "aka (red) + shiro (white) miso, tofu, seaweed, scallions",
-                "Price": 2.25
-            },
-        ]},
-    ]
-
     const [openModal, setOpenModal] = useState(false)
     const [selectedItem, setSelectedItem] = useState(null)
-
 
     // processes the list of item objects
     function processItem(item_type_obj, key){
@@ -129,6 +39,8 @@ export default function Item() {
         return item_jsx
     }
 
+    // by default, do not render the Modal unless user clicks on the item
+    // which then selectedItem is updated and modal is rendered
     function renderItemModal(){
         if (selectedItem == null){
             return null
@@ -143,7 +55,6 @@ export default function Item() {
         }
     }
     
-
     // passes list of item objects per item type to the processItem function
     const item_type_component = menu_item.map(item_type_obj => {
         const item_type = Object.keys(item_type_obj)[0]
