1527809143468a48b7cf20f8f121ad5608d5ce66
Jun Ahn <wtg6471@gmail.com>
2023-10-12 20:46:17 -0700

    added heading, resized the nav bar and footer

--- components/menu/heading.js
+++ components/menu/heading.js
@@ -1,10 +1,20 @@
 import styles from 'components/stylesheets/heading.module.css'
+import Image from 'next/image'
 
 export default function Heading() {
     return(
         <div className={styles.heading}>
+            <Image
+                src="/logo.jpeg"
+                width="250"
+                height="250"
+                alt="logo"
+                className={styles.logo}
+            />
             <div className={styles["event-notes"]}>
-                <p></p>
+                <p>Substitutions and/or any special request may not be accommodatable. Aburi Sake Don is not available for take-out due to food safety concerns.</p>
+                <p>Foods from restaurant that are picked up or delivered should be consumed immediately. Delivery or take-out foods purchased in advance for eating later should be maintained at proper temperatures until eaten.</p>
+                <p>We truly value the quality of our food and safety of our customers.</p>
             </div>
         </div>
     )
