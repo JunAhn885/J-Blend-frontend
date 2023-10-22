c9bc0463336538ab775b12ac08a55cee158efbb7
Jun Ahn <wtg6471@gmail.com>
2023-10-22 09:35:06 -0700

    installed react motion package

--- components/menu/item-modal.tsx
+++ components/menu/item-modal.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope */
 import styles from "components/menu/stylesheets/item-modal.module.css";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { formatCurrency } from "utilities/formatCurrency.ts";
 import { MenuItem } from "@/data/menu_item";
 import useCounter from "hooks/useCounter.ts";
 import calTotalPrice from "@/utilities/calTotalPrice";
 import { useCartContext } from "@/context/cartContext";
 import QuantityButton from "../frequentlyUsed/quantityButton";
+import { CSSTransition } from "react-transition-group";
 
 export default function ItemModal({
   // props
@@ -21,6 +22,7 @@ export default function ItemModal({
   item_obj: MenuItem;
 }) {
   const { count, setCount, increment, decrement } = useCounter(1);
+  const [shouldRender, setShouldRender] = useState(open);
   const [instruction, setInstruction] = useState("");
   const price: number = item_obj["Price"];
   const description: string = item_obj["Description"];
@@ -28,68 +30,82 @@ export default function ItemModal({
   const id: number = item_obj["id"];
   const { addItemToCart } = useCartContext();
 
-  // do not display the modal if open is false
-  if (open == false) {
-    return null;
-  }
+  useEffect(() => {
+    if (open) setShouldRender(true);
+  }, [open]);
+
+  const onAnimationEnd = () => {
+    if (!open) setShouldRender(false);
+  };
 
   return (
-    <div
-      onClick={() => {
-        setOpen();
-        setCount(1);
-      }}
-      className={styles.overlay}
-    >
+    open && (
       <div
-        onClick={(e) => {
-          e.stopPropagation();
+        onClick={() => {
+          setOpen();
+          setCount(1);
         }}
-        className={styles.modal}
+        className={styles.overlay}
       >
-        <div className={styles["modal-content"]}>
-          <Image src="/chirashi.jpeg" width="300" height="300" alt="Chirashi" />
-          <h1>{itemName}</h1>
-          <h2>{formatCurrency(price)}</h2>
-          <h3>{description}</h3>
-
-          <QuantityButton
-            count={count}
-            increment={increment}
-            decrement={decrement}
-          />
-          <form className={styles["special-instruction"]}>
-            <label>Special Instructions:</label>
-            <input
-              type="text"
-              id="instruction"
-              name="special instruction"
-              onChange={(e) => {
-                setInstruction(e.target.value);
-              }}
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          className={styles.modal}
+        >
+          <div className={styles["modal-content"]}>
+            <Image
+              src="/chirashi.jpeg"
+              width="300"
+              height="300"
+              alt="Chirashi"
             />
-          </form>
-          <div className={styles["exit-add-button-container"]}>
-            <button
-              onClick={() => {
-                setOpen();
-                setCount(1);
-              }}
-              className={styles["exit-button"]}
-            >
-              x
-            </button>
-            <button
-              className={styles["add-button"]}
-              onClick={() =>
-                addItemToCart({ id: id, quantity: count, request: instruction })
-              }
-            >
-              {`Add to order ${formatCurrency(calTotalPrice(price, count))}`}
-            </button>
+            <h1>{itemName}</h1>
+            <h2>{formatCurrency(price)}</h2>
+            <h3>{description}</h3>
+
+            <QuantityButton
+              count={count}
+              increment={increment}
+              decrement={decrement}
+            />
+            <form className={styles["special-instruction"]}>
+              <label>Special Instructions:</label>
+              <input
+                type="text"
+                id="instruction"
+                name="special instruction"
+                onChange={(e) => {
+                  setInstruction(e.target.value);
+                }}
+              />
+            </form>
+            <div className={styles["exit-add-button-container"]}>
+              <button
+                onClick={() => {
+                  setOpen();
+                  setCount(1);
+                }}
+                className={styles["exit-button"]}
+              >
+                x
+              </button>
+              <button
+                className={styles["add-button"]}
+                onClick={() =>
+                  addItemToCart({
+                    id: id,
+                    quantity: count,
+                    request: instruction,
+                  })
+                }
+              >
+                {`Add to order ${formatCurrency(calTotalPrice(price, count))}`}
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
