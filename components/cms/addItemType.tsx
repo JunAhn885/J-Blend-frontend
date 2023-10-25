fd4f10eb713e2a339294d2385a65fed1652c72e4
Jun Ahn <wtg6471@gmail.com>
2023-10-24 21:03:37 -0700

    added react logic for adding item type

--- components/cms/addItemType.tsx
+++ components/cms/addItemType.tsx
@@ -1,7 +1,20 @@
+"use client";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 export default function AddItemType() {
+  const [itemTypeList, setItemTypeList] = useState<string[]>([]);
+  const [itemType, setItemType] = useState<string>("");
+
+  function addItemType(): void {
+    let itemTypeListCopy = [...itemTypeList];
+    itemTypeListCopy.push(itemType);
+    setItemTypeList(itemTypeListCopy);
+    setItemType("");
+  }
+
+  console.log(itemTypeList);
   return (
     <div>
       <h1>To begin, please add item types below:</h1>
@@ -9,8 +22,16 @@ export default function AddItemType() {
         ex: "burgers, fries, drinks, and sweets" for a hamburger restaurant
       </h2>
       <h2>Please add one item type at a time</h2>
-      <TextField id="outlined-basic" label="type" variant="outlined" />
-      <Button variant="contained">Contained</Button>
+      <TextField
+        id="outlined-basic"
+        label="type"
+        variant="outlined"
+        value={itemType}
+        onChange={(e) => setItemType(e.target.value)}
+      />
+      <Button variant="contained" onClick={addItemType}>
+        Add
+      </Button>
     </div>
   );
 }
